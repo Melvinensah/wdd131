619d041514cad06ace26876ebe2c6abf9d676e2e
Melvinensah <melvinbunduka@gmail.com>
2026-05-19 02:54:21 +0000

    updated

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,16 +1,3 @@
-// const YearElement = document.getElementById("currentyear");
-// const currentYear = new Date().getFullYear();
-
-// if (YearElement) {
-//     YearElement.textContent = currentYear;
-// }
-
-// const updateElement = document.getElementById("lastModified");
-// const lastUpdated = document.lastModified;
-
-// if (updateElement){
-//     updateElement.textContent = "last modified   " + lastUpdated; 
-// }
 
 document.addEventListener("DOMContentLoaded", () => {
     
