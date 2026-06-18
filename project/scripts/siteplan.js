fb0d0036e5115f5c23f8bb49112f3240de4e0874
Melvinensah <melvinbunduka@gmail.com>
2026-06-18 08:15:37 +0000

    updated

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -1,4 +1,18 @@
 // --- BBEM Technical Limited Site Plan Logic Engine ---
+
+const YearElement = document.getElementById("currentyear");
+const currentYear = new Date().getFullYear();
+
+if (YearElement) {
+    YearElement.textContent = currentYear;
+}
+
+const updateElement = document.getElementById("lastModified");
+const lastUpdated = document.lastModified;
+
+if (updateElement){
+    updateElement.textContent = "last modified   " + lastUpdated; 
+}
 document.addEventListener("DOMContentLoaded", () => {
   displaySitePlanMetadata();
 });
